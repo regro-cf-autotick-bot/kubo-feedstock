22a1c5cabbe855f69a5e58e396056d96c5db59e9
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-19 17:05:17 -0500

    win go stdlib licenses ignores

--- recipe/build_library_licenses.sh
+++ recipe/build_library_licenses.sh
@@ -5,9 +5,39 @@ export LIBRARY_LICENSES_PATH="$SRC_DIR/library_licenses/"
 
 go-licenses save \
     "." \
-    --save_path "$LIBRARY_LICENSES_PATH" \
-    2>&1 \
-    | tee "$SRC_DIR/go-licenses.log" \
-    || echo "ignoring errors until go-licenses --ignore is available"
+    --ignore=archive/tar \
+    --ignore=archive/zip \
+    --ignore=cmp \
+    --ignore=compress/gzip \
+    --ignore=compress/zlib \
+    --ignore=container/heap \
+    --ignore=container/list \
+    --ignore=crypto/hmac \
+    --ignore=crypto/tls \
+    --ignore=crypto/x509 \
+    --ignore=embed \
+    --ignore=encoding/asn1 \
+    --ignore=expvar \
+    --ignore=go/format \
+    --ignore=go/token \
+    --ignore=hash/crc32 \
+    --ignore=hash/fnv \
+    --ignore=io/fs \
+    --ignore=io/ioutil \
+    --ignore=log/slog \
+    --ignore=math/cmplx \
+    --ignore=math/rand/v2 \
+    --ignore=mime \
+    --ignore=mime/multipart \
+    --ignore=net/netip \
+    --ignore=net/textproto \
+    --ignore=os/exec \
+    --ignore=runtime/metrics \
+    --ignore=runtime/pprof \
+    --ignore=slices \
+    --ignore=testing \
+    --ignore=text/template \
+    --ignore=unicode \
+    --save_path "$LIBRARY_LICENSES_PATH"
 
 find "$LIBRARY_LICENSES_PATH"
