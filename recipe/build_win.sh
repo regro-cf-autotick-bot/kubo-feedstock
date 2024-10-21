e2159d829a9fe038a5bf8872ddff6953cfabc449
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-21 07:56:28 -0500

    MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.43.0, and conda-forge-pinning 2024.10.21.11.59.29

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -2,7 +2,7 @@
 set -eux
 
 export GOPATH="$( pwd )"
-export CGO_ENABLED=0
+export CGO_ENABLED=1
 export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external -mod=vendor -modcacherw"
 
 # https://github.com/conda-forge/kubo-feedstock/issues/5
@@ -10,18 +10,18 @@ export GOTMPDIR="$(pwd)/tmp"
 
 mkdir -p "${GOTMPDIR}"
 
-# omissions from the unix build, needs investigation
-# export GOTAGS="openssl"
-# export CGO_CFLAGS="${CFLAGS}"
-# export CGO_CXXFLAGS="${CPPFLAGS}"
-# export CGO_LDFLAGS="${LDFLAGS}"
+omissions from the unix build, needs investigation
+export GOTAGS="openssl"
+export CGO_CFLAGS="${CFLAGS}"
+export CGO_CXXFLAGS="${CPPFLAGS}"
+export CGO_LDFLAGS="${LDFLAGS}"
 
 module='github.com/ipfs/kubo'
 
 make -C "src/${module}" install nofuse
 
 pushd "src/${module}"
-    bash $RECIPE_DIR/build_library_licenses.sh
+    bash "${RECIPE_DIR}/build_library_licenses.sh"
 popd
 
 rm -rf "${GOTMPDIR}"
